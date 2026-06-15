b2261007c44b4be20034091dfe095f0f6194a371
phelistus003 <phelistuskangu93@gmail.com>
2026-06-15 15:21:17 -0400

    Implement and register CarMake and CarModel models

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,13 +1,51 @@
 # Uncomment the following imports before adding the Model code
 
-# from django.db import models
-# from django.utils.timezone import now
-# from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from django.utils.timezone import now
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 # Create your models here.
+class CarMake(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True, null=True)
+    country_of_origin = models.CharField(max_length=50, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
 
 # <HINT> Create a Car Make model `class CarMake(models.Model)`:
+class CarModel(models.Model):
+    # Many-to-one relationship to CarMake
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE, related_name="car_models")
+    
+    # Dealer ID referring to a dealer created in Cloudant database
+    dealer_id = models.IntegerField()
+    
+    name = models.CharField(max_length=100)
+    
+    # Type choices matching standard conventions
+    SEDAN = 'Sedan'
+    SUV = 'SUV'
+    WAGON = 'Wagon'
+    
+    CAR_TYPES = [
+        (SEDAN, 'Sedan'),
+        (SUV, 'SUV'),
+        (WAGON, 'Wagon'),
+    ]
+    car_type = models.CharField(max_length=20, choices=CAR_TYPES, default=SEDAN)
+    
+    # Year field strictly validated between 2015 and 2023 as specified by the hint
+    year = models.IntegerField(
+        validators=[
+            MinValueValidator(2015),
+            MaxValueValidator(2023)
+        ]
+    )
+
+    def __str__(self):
+        return f"{self.car_make.name} {self.name}"
 # - Name
 # - Description
 # - Any other fields you would like to include in car make model
--- server/djangoenv/bin/activate.fish
+++ server/djangoenv/bin/activate.fish
@@ -1,31 +1,58 @@
-# This file must be used with "source <venv>/bin/activate.fish" *from fish*
-# (https://fishshell.com/); you cannot run it directly.
+# This file must be used using `source bin/activate.fish` *within a running fish ( http://fishshell.com ) session*.
+# Do not run it directly.
 
-function deactivate  -d "Exit virtual environment and return to normal shell environment"
+function deactivate -d 'Exit virtualenv mode and return to the normal environment.'
     # reset old environment variables
     if test -n "$_OLD_VIRTUAL_PATH"
         set -gx PATH $_OLD_VIRTUAL_PATH
         set -e _OLD_VIRTUAL_PATH
     end
+
+    if test -n ''
+      if test -n "$_OLD_VIRTUAL_TCL_LIBRARY";
+        set -gx TCL_LIBRARY "$_OLD_VIRTUAL_TCL_LIBRARY";
+        set -e _OLD_VIRTUAL_TCL_LIBRARY;
+      else;
+        set -e TCL_LIBRARY;
+      end
+    end
+    if test -n ''
+      if test -n "$_OLD_VIRTUAL_TK_LIBRARY";
+        set -gx TK_LIBRARY "$_OLD_VIRTUAL_TK_LIBRARY";
+        set -e _OLD_VIRTUAL_TK_LIBRARY;
+      else;
+        set -e TK_LIBRARY;
+      end
+    end
+
+    if test -n "$_OLD_PKG_CONFIG_PATH"
+        set -gx PKG_CONFIG_PATH "$_OLD_PKG_CONFIG_PATH"
+        set -e _OLD_PKG_CONFIG_PATH
+    end
+
     if test -n "$_OLD_VIRTUAL_PYTHONHOME"
-        set -gx PYTHONHOME $_OLD_VIRTUAL_PYTHONHOME
+        set -gx PYTHONHOME "$_OLD_VIRTUAL_PYTHONHOME"
         set -e _OLD_VIRTUAL_PYTHONHOME
     end
 
     if test -n "$_OLD_FISH_PROMPT_OVERRIDE"
+       and functions -q _old_fish_prompt
+        # Set an empty local `$fish_function_path` to allow the removal of `fish_prompt` using `functions -e`.
+        set -l fish_function_path
+
+        # Erase virtualenv's `fish_prompt` and restore the original.
+        functions -e fish_prompt
+        functions -c _old_fish_prompt fish_prompt
+        functions -e _old_fish_prompt
         set -e _OLD_FISH_PROMPT_OVERRIDE
-        # prevents error when using nested fish instances (Issue #93858)
-        if functions -q _old_fish_prompt
-            functions -e fish_prompt
-            functions -c _old_fish_prompt fish_prompt
-            functions -e _old_fish_prompt
-        end
     end
 
     set -e VIRTUAL_ENV
     set -e VIRTUAL_ENV_PROMPT
-    if test "$argv[1]" != "nondestructive"
+
+    if test "$argv[1]" != 'nondestructive'
         # Self-destruct!
+        functions -e pydoc
         functions -e deactivate
     end
 end
@@ -34,36 +61,61 @@ end
 deactivate nondestructive
 
 set -gx VIRTUAL_ENV /home/project/xrwvm-fullstack_developer_capstone/server/djangoenv
+if string match -qr 'CYGWIN|MSYS|MINGW' (uname)
+    set -gx VIRTUAL_ENV (cygpath -u $VIRTUAL_ENV)
+end
+
+set -gx _OLD_PKG_CONFIG_PATH "$PKG_CONFIG_PATH"
+set -gx PKG_CONFIG_PATH "$VIRTUAL_ENV/lib/pkgconfig:$PKG_CONFIG_PATH"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
-set -gx PATH "$VIRTUAL_ENV/"bin $PATH
+set -gx PATH "$VIRTUAL_ENV"'/'bin $PATH
 
-# Unset PYTHONHOME if set.
+if test -n ''
+  if set -q TCL_LIBRARY;
+    set -gx _OLD_VIRTUAL_TCL_LIBRARY $TCL_LIBRARY;
+  end
+  set -gx TCL_LIBRARY ''''
+end
+if test -n ''
+  if set -q TK_LIBRARY;
+    set -gx _OLD_VIRTUAL_TK_LIBRARY $TK_LIBRARY;
+  end
+  set -gx TK_LIBRARY ''''
+end
+
+# Prompt override provided?
+# If not, just use the environment name.
+if test -n ''
+    set -gx VIRTUAL_ENV_PROMPT ''
+else
+    set -gx VIRTUAL_ENV_PROMPT (basename "$VIRTUAL_ENV")
+end
+
+# Unset `$PYTHONHOME` if set.
 if set -q PYTHONHOME
     set -gx _OLD_VIRTUAL_PYTHONHOME $PYTHONHOME
     set -e PYTHONHOME
 end
 
-if test -z "$VIRTUAL_ENV_DISABLE_PROMPT"
-    # fish uses a function instead of an env var to generate the prompt.
+function pydoc
+    python -m pydoc $argv
+end
 
-    # Save the current fish_prompt function as the function _old_fish_prompt.
+if test -z "$VIRTUAL_ENV_DISABLE_PROMPT"
+    # Copy the current `fish_prompt` function as `_old_fish_prompt`.
     functions -c fish_prompt _old_fish_prompt
 
-    # With the original prompt function renamed, we can override with our own.
     function fish_prompt
-        # Save the return status of the last command.
         set -l old_status $status
+        # Run the user's prompt first; it might depend on (pipe)status.
+        set -l prompt (_old_fish_prompt)
 
-        # Output the venv prompt; color taken from the blue of the Python logo.
-        printf "%s%s%s" (set_color 4B8BBE) '(djangoenv) ' (set_color normal)
+        printf '(%s) ' $VIRTUAL_ENV_PROMPT
 
-        # Restore the return status of the previous command.
+        string join -- \n $prompt # handle multi-line prompts
         echo "exit $old_status" | .
-        # Output the original/"old" prompt.
-        _old_fish_prompt
     end
 
     set -gx _OLD_FISH_PROMPT_OVERRIDE "$VIRTUAL_ENV"
-    set -gx VIRTUAL_ENV_PROMPT '(djangoenv) '
 end
